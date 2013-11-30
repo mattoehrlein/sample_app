65f0781e6f2a5263a935e8bc6dfe9954940f4a66
Matt Oehrlein <matt.oehrlein@gmail.com>
2013-11-30 04:41:56 +0000

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,25 +4,49 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_selector('h1', :text => 'Sample App')
+  end
+    it "should have the right title" do
+	  visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_content('Help')
+      expect(page).to have_selector('h1', :text => 'Help')
+    end
+     it "should have the right title" do
+	  visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
     describe "About page" do
 
-    it "should have the content 'About Us'" do
+    it "should have the h1 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_content('About Us')
+      expect(page).to have_selector('h1', :text => 'About Us')
+    end
+     it "should have the right title" do
+	  visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+    describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector('h1', :text => 'Contact')
+    end
+     it "should have the right title" do
+    visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
